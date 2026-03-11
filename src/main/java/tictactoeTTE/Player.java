87a93d9b16f7d8fed1637a45c69a5817e75607f0
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-03-11 09:10:56 -0600

    Added takeTurn initial method and updated constructor

--- src/main/java/tictactoeTTE/Player.java
+++ src/main/java/tictactoeTTE/Player.java
@@ -1,12 +1,34 @@
 package tictactoeTTE;
 
+import java.util.LinkedList;
+
 public class Player {
     private String playerName;
-    private IMove movement;
+    private String playerSymbol;
+    private Movement movementStyle;
+    private LinkedList<int[]> moveHistory;
 
 
-    public Player(String playerName, IMove movementStyle){
+    public Player(String playerName, String symbol, Movement movementStyle){
         this.playerName = playerName;
-        this.movement = movementStyle;
+        this.playerSymbol = symbol;
+        this.movementStyle = movementStyle;
+        this.moveHistory = new LinkedList<>();
+    }
+
+    public void takeTurn(GameBoard gameBoard){
+        int[] nextMove = movementStyle.getNextMove(gameBoard, this);
+
+        if (moveHistory.size() == 3){
+            // Get oldest movement (head of list)
+            // Update observer to display special symbol on board at oldest symbol location
+            // Get next requested location
+            // Remove oldest movement
+            // Update observer
+        }
+        else{
+            // Get next requested movement
+            // Update observer to update board display
+        }
     }
 }

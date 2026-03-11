23e059ac41bbc83d02bf887246869a6eed1e80a7
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-03-11 09:14:26 -0600

    Added initial design of board layout

--- src/main/java/tictactoeTTE/GameBoard.java
+++ src/main/java/tictactoeTTE/GameBoard.java
@@ -1,4 +1,13 @@
 package tictactoeTTE;
 
 public class GameBoard {
+
+    private int MAX_ROWS = 3;
+    private int MAX_COLUMNS = 3;
+
+    private int[][] boardLayout;
+
+    public GameBoard() {
+        boardLayout = new int[MAX_ROWS][MAX_COLUMNS];
+    }
 }

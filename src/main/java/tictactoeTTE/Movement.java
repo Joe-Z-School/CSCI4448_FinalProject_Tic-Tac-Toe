c6abbd82f1ab2cc6b431ec41780fb85ca47ea4d6
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-03-11 09:11:36 -0600

    Added initial concepts for superclass on movements

---
+++ src/main/java/tictactoeTTE/Movement.java
@@ -0,0 +1,17 @@
+package tictactoeTTE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Movement implements IMove{
+
+
+    List<int[]> getAvailableMoves(GameBoard board, Player player){
+        List<int[]> availableMoves = new ArrayList<>();
+        // Get spots available
+        return availableMoves;
+    };
+
+
+    public abstract int[] getNextMove(GameBoard board, Player player);
+}

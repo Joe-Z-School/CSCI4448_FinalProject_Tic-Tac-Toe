bb6d7fe74eb6fd6c5933c827010b1f38adde03ca
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-03-12 09:44:39 -0600

    Added methods to get symbol and name of player

--- src/main/java/tictactoeTTE/Players/Player.java
+++ src/main/java/tictactoeTTE/Players/Player.java
@@ -3,35 +3,24 @@ package tictactoeTTE.Players;
 import tictactoeTTE.GameBoard;
 import tictactoeTTE.Movement;
 
-import java.util.LinkedList;
-
 public abstract class Player {
     private String playerName;
     private String playerSymbol;
     private Movement movementStyle;
-    private LinkedList<int[]> moveHistory;
 
 
     public Player(String playerName, String symbol, Movement movementStyle){
         this.playerName = playerName;
         this.playerSymbol = symbol;
         this.movementStyle = movementStyle;
-        this.moveHistory = new LinkedList<>();
     }
 
     public void takeTurn(GameBoard gameBoard){
         int[] nextMove = movementStyle.getNextMove(gameBoard, this);
 
-        if (moveHistory.size() == 3){
-            // Get oldest movement (head of list)
-            // Update observer to display special symbol on board at oldest symbol location
-            // Get next requested location
-            // Remove oldest movement
-            // Update observer
-        }
-        else{
-            // Get next requested movement
-            // Update observer to update board display
-        }
+        Boolean moveSuccessful = gameBoard.doMovement(nextMove[0], nextMove[1], this);
     }
+
+    public String getSymbol(){ return playerSymbol; }
+    public String getPlayerName(){ return playerName; }
 }

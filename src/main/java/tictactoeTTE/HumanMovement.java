22d6f4df87aea624cdfbe45ab3159b894c195025
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-03-12 11:56:29 -0600

    Added input for user to specify spot

--- src/main/java/tictactoeTTE/HumanMovement.java
+++ src/main/java/tictactoeTTE/HumanMovement.java
@@ -5,9 +5,18 @@ import tictactoeTTE.Players.Player;
 public class HumanMovement extends Movement {
 
     public int[] getNextMove(GameBoard board, Player playerMoving) {
-        return null;
-        /*
-        Get movement provided by player
-         */
+        int rowChosen = -1;
+        int columnChosen = -1;
+
+        while(true) {
+            rowChosen = Integer.parseInt(System.console().readLine("Enter a row: "));
+            columnChosen = Integer.parseInt(System.console().readLine("Enter a column: "));
+            if (board.isValidSpot(rowChosen, columnChosen)) {
+                break;
+            }
+            System.out.println("Invalid spot chosen! Please try again.");
+        }
+
+        return new int[]{rowChosen, columnChosen};
     }
 }

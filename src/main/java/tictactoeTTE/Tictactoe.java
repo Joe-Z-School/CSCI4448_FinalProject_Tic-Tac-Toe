0cb1d470e3f4c1f18122c99b598c5b7f42bb1370
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-03-12 11:03:35 -0600

    Added method for play, constructor, isOver, getCurrentPlayer, and switch turn

--- src/main/java/tictactoeTTE/Tictactoe.java
+++ src/main/java/tictactoeTTE/Tictactoe.java
@@ -5,6 +5,38 @@ import tictactoeTTE.Players.Player;
 public class Tictactoe {
 
     private Boolean gameIsOver = false;
-    private Player winner;
+    private GameBoard board;
+    private Player playerOne;
+    private Player playerTwo;
+    private Player currentPlayer;
+
+    public Tictactoe(GameBoard board, Player playerOne, Player playerTwo) {
+        this.board = board;
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+        this.currentPlayer = playerOne; // Default to player one starting
+    }
+
+    public void play() {
+        while (!gameIsOver) {
+            board.displayBoard();
+            currentPlayer.takeTurn(board);
+            if (board.checkIfWinner(currentPlayer)) {
+                gameIsOver = true;
+                System.out.println("GAME OVER");
+                System.out.println("Winner: " + currentPlayer);
+            } else {
+                switchTurn();
+            }
+        }
+    }
+
+    public Boolean isOver(){ return gameIsOver; }
+
+    public Player getCurrentPlayer(){ return currentPlayer; }
+
+    private void switchTurn() {
+        currentPlayer = currentPlayer.equals(playerOne) ? playerTwo : playerOne;
+    }
 
 }

615b034e058c0d09006697121f4613a5d9b19025
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-03-12 11:02:46 -0600

    Added initial test for gameplay

--- src/test/java/tictactoeTTE/TictactoeTest.java
+++ src/test/java/tictactoeTTE/TictactoeTest.java
@@ -1,4 +1,25 @@
 package tictactoeTTE;
 
+import org.junit.jupiter.api.Test;
+import tictactoeTTE.Players.Player;
+import tictactoeTTE.Players.PlayerFactory;
+
 public class TictactoeTest {
+    String playerOneName = "Joe";
+    String playerTwoName = "Jane";
+    String playerOneSymbol = "X";
+    String playerTwoSymbol = "O";
+
+    @Test
+    public void testTicTacToe() {
+        PlayerFactory playerFactory = new PlayerFactory();
+        Player playerOne = playerFactory.createHumanPlayer(playerOneName, playerOneSymbol);
+        Player playerTwo = playerFactory.createComputerPlayer(playerTwoSymbol);
+        GameBoard board = new GameBoard();
+        Tictactoe game = new Tictactoe(board, playerOne, playerTwo);
+
+        game.play();
+
+        Boolean gameIsOverResult = game.isOver();
+    }
 }

a17798f729cfaf9daae5814ef914295503dee654
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-03-12 09:45:26 -0600

    Changed factory to instead hold separate methods for creating humans and computer players

--- src/main/java/tictactoeTTE/Players/PlayerFactory.java
+++ src/main/java/tictactoeTTE/Players/PlayerFactory.java
@@ -4,13 +4,13 @@ import tictactoeTTE.AIMovement;
 import tictactoeTTE.HumanMovement;
 
 public class PlayerFactory {
+    private final String COMPUTER_DEFAULT_NAME = "Computer";
 
-    public Player createPlayer(String playerName, String symbol) {
-        if (playerName.equalsIgnoreCase("AI")){
-            return new Player("AI", symbol, new AIMovement());
-        }
-        else {
-            return new Player(playerName, symbol, new HumanMovement());
-        }
+    public Player createHumanPlayer(String playerName, String symbol) {
+            return new Human(playerName, symbol, new HumanMovement());
+    }
+
+    public Player createComputerPlayer(String symbol){
+        return new Computer(COMPUTER_DEFAULT_NAME, symbol, new AIMovement());
     }
 }

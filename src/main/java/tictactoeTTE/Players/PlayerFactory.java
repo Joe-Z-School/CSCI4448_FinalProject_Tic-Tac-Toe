c1acd857cf6fbebfdb9abdccff4b77b668c3c95f
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-04-20 15:35:26 -0600

    Added new player style of simple computer

--- src/main/java/tictactoeTTE/Players/PlayerFactory.java
+++ src/main/java/tictactoeTTE/Players/PlayerFactory.java
@@ -1,6 +1,8 @@
 package tictactoeTTE.Players;
 
-import tictactoeTTE.Movement.AIMovement;
+import tictactoeTTE.Movement.Movement;
+import tictactoeTTE.Movement.SimpleComputerMovement;
+import tictactoeTTE.Movement.SmartComputerMovement;
 import tictactoeTTE.Movement.HumanMovement;
 
 public class PlayerFactory {
@@ -10,7 +12,8 @@ public class PlayerFactory {
             return new Human(playerName, symbol, new HumanMovement());
     }
 
-    public Player createComputerPlayer(String symbol){
-        return new Computer(COMPUTER_DEFAULT_NAME, symbol, new AIMovement());
+    public Player createComputerPlayer(String symbol, String difficulty){
+        Movement strategy = difficulty.equals("Smart") ? new SmartComputerMovement() : new SimpleComputerMovement();
+        return new Computer(COMPUTER_DEFAULT_NAME, symbol, strategy);
     }
 }

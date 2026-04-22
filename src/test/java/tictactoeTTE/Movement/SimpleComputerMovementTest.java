f97f6268cbbd99e097f7be68817d0ede8627a114
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-04-22 09:45:39 -0600

    initial add

--- src/test/java/tictactoeTTE/Movement/SimpleComputerMovementTest.java
+++ src/test/java/tictactoeTTE/Movement/SimpleComputerMovementTest.java
@@ -1,4 +1,37 @@
 package tictactoeTTE.Movement;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tictactoeTTE.GameBoard;
+import tictactoeTTE.Players.Player;
+import tictactoeTTE.Players.PlayerFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class SimpleComputerMovementTest {
+    GameBoard gameBoard;
+    PlayerFactory playerFactory;
+
+    String humanPlayerName = "Joe";
+    String playerOneSymbol = "X";
+    String playerTwoSymbol = "O";
+    String computerDifficulty = "Simple";
+    Player computerPlayer;
+    Player humanPlayer;
+    int DEFAULT_BOARD_SIZE = 3;
+
+    @BeforeEach
+    void setUp() {
+        gameBoard = new GameBoard(DEFAULT_BOARD_SIZE);
+        playerFactory = new PlayerFactory();
+        humanPlayer = playerFactory.createHumanPlayer(humanPlayerName, playerOneSymbol);
+        computerPlayer = playerFactory.createComputerPlayer(playerTwoSymbol,computerDifficulty);
+    }
+
+    @Test
+    void computerMovesRandomly(){
+
+    }
+
 }

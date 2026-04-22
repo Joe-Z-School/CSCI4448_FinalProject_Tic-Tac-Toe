208a96b8bd80fad834ea02b97b161c8570986af1
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-04-22 09:44:22 -0600

    Changed tests to reflect new name that contains difficulty setting

--- src/test/java/tictactoeTTE/Players/PlayerFactoryTest.java
+++ src/test/java/tictactoeTTE/Players/PlayerFactoryTest.java
@@ -18,10 +18,20 @@ class PlayerFactoryTest {
     }
 
     @Test
-    void createComputerPlayerTest() {
-        String expectedComputerName = "Computer";
+    void createSimpleComputerPlayerTest() {
+        String expectedComputerName = "Simple Computer";
         String compPlayerSymbol = "O";
         String computerDifficulty = "Simple";
+        Player computerPlayer = playerFactory.createComputerPlayer(compPlayerSymbol,computerDifficulty);
+
+        assertEquals(expectedComputerName, computerPlayer.getPlayerName());
+    }
+
+    @Test
+    void createSmartComputerPlayerTest() {
+        String expectedComputerName = "Smart Computer";
+        String compPlayerSymbol = "O";
+        String computerDifficulty = "Smart";
         Player computerPlayer = playerFactory.createComputerPlayer(compPlayerSymbol,computerDifficulty);
 
         assertEquals(expectedComputerName, computerPlayer.getPlayerName());

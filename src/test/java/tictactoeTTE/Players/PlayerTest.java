a39df301f743c3442e57b367408199f89f864507
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-04-22 10:35:20 -0600

    Added tests for method coverage

--- src/test/java/tictactoeTTE/Players/PlayerTest.java
+++ src/test/java/tictactoeTTE/Players/PlayerTest.java
@@ -1,4 +1,34 @@
 package tictactoeTTE.Players;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class PlayerTest {
+
+    PlayerFactory playerFactory = new PlayerFactory();
+
+    Player humanPlayer;
+    Player computerPlayer;
+    String humanPlayerName = "Joe";
+    String DEFAULT_HUMAN_SYMBOL = "X";
+    String DEFAULT_COMPUTER_SYMBOL = "O";
+    String computerDifficulty = "Simple";
+
+    @Test
+    void isHumanTest(){
+        computerPlayer = playerFactory.createComputerPlayer(DEFAULT_COMPUTER_SYMBOL, computerDifficulty);
+
+        assertFalse(computerPlayer.isHuman());
+        assertTrue(computerPlayer.isComputer());
+    }
+
+    @Test
+    void isComputerTest(){
+        humanPlayer = playerFactory.createHumanPlayer(DEFAULT_HUMAN_SYMBOL, humanPlayerName);
+
+        assertFalse(humanPlayer.isComputer());
+        assertTrue(humanPlayer.isHuman());
+    }
 }

7bb5750a13214192deda85a32bff29bfa1508ebb
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-04-22 15:35:16 -0600

    Changed input to read from scanner for testing abilities

--- src/main/java/tictactoeTTE/Movement/HumanMovement.java
+++ src/main/java/tictactoeTTE/Movement/HumanMovement.java
@@ -2,16 +2,21 @@ package tictactoeTTE.Movement;
 
 import tictactoeTTE.GameBoard;
 import tictactoeTTE.Players.Player;
+import java.util.Scanner;
 
 public class HumanMovement extends Movement {
+    private final Scanner in = new Scanner(System.in);
 
     public int[] getNextMove(GameBoard board, Player playerMoving) {
         int rowChosen = -1;
         int columnChosen = -1;
 
         while(true) {
-            rowChosen = Integer.parseInt(System.console().readLine("Enter a row: "));
-            columnChosen = Integer.parseInt(System.console().readLine("Enter a column: "));
+            System.out.print("Enter a row: ");
+            rowChosen = in.nextInt();
+            System.out.print("Enter a column: ");
+            columnChosen = in.nextInt();
+
             if (board.isValidSpot(rowChosen, columnChosen)) {
                 break;
             }

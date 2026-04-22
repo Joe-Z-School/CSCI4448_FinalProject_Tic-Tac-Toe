1e211a58ffef20deb02c768f8345c6fe51214194
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-04-22 14:15:05 -0600

    Added tests to cover getGameboard and to test timer for swing ui

--- src/test/java/tictactoeTTE/TictactoeTest.java
+++ src/test/java/tictactoeTTE/TictactoeTest.java
@@ -4,21 +4,23 @@ import org.junit.jupiter.api.Test;
 import tictactoeTTE.Players.Player;
 import tictactoeTTE.Players.PlayerFactory;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TictactoeTest {
     String playerOneSymbol = "X";
     String playerTwoSymbol = "O";
     String computerDifficulty = "Smart";
     int DEFAULT_BOARD_SIZE = 3;
+    PlayerFactory playerFactory = new PlayerFactory();
+    Player playerOne = playerFactory.createComputerPlayer(playerOneSymbol,computerDifficulty);
+    Player playerTwo = playerFactory.createComputerPlayer(playerTwoSymbol,computerDifficulty);
+    GameBoard board = new GameBoard(DEFAULT_BOARD_SIZE);
+    Tictactoe game = new Tictactoe(board, playerOne, playerTwo);
 
 
     @Test
     public void testTicTacToeWithTwoComputers() {
-        PlayerFactory playerFactory = new PlayerFactory();
-        Player playerOne = playerFactory.createComputerPlayer(playerOneSymbol,computerDifficulty);
-        Player playerTwo = playerFactory.createComputerPlayer(playerTwoSymbol,computerDifficulty);
-        GameBoard board = new GameBoard(DEFAULT_BOARD_SIZE);
-        Tictactoe game = new Tictactoe(board, playerOne, playerTwo);
-
 
         while(true) {
             int[] playerOneMove = playerOne.getMove(board);
@@ -36,7 +38,27 @@ public class TictactoeTest {
         }
 
         Boolean gameIsOverResult = game.isOver();
-
         assert(gameIsOverResult);
+    }
+
+    @Test
+    void testGetGameBoard() {
+        assertEquals(board, game.getGameBoard());
+    }
+
+    @Test
+    void testComputerSleepTimer() throws InterruptedException {
+        board.doMovement(0,0, playerOne);
+        board.doMovement(2,2, playerTwo);
+
+        game.handleMove(0,1);
+
+        Thread.sleep(3000);
+
+        game.getGameBoard().displayBoard();
+
+        String[][] gameBoardLayout = board.getBoardLayout();
+
+        assertEquals(playerTwo.getSymbol(), gameBoardLayout[0][2]);
     }
 }
